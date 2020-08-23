e77ae62fa1569b08d6d0576225d250f429e8bbb9
youyang <dbdid0064@gmail.com>
2020-08-23 18:03:57 +0900

    0823

--- app/src/main/java/com/example/bestfood/MainActivity.java
+++ app/src/main/java/com/example/bestfood/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present
+        // Inflate the menu; this adds items to the action bar  it is present
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
